fff54bc84063285eb0da9d6b2e0ca06fa874a14b
Adam Huss <adamhuss13@gmail.com>
2024-04-15 17:34:32 -0500

    added find all products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,8 +4,12 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 // The `/api/products` endpoint
 
 // get all products
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
   // find all products
+  const results = await Product.findAll({
+    include: [{ model: Category }, {model: Tag}]
+  });
+  res.json(results);
   // be sure to include its associated Category and Tag data
 });
 
