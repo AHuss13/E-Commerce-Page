d0a41dedeb8af45513f93d68803bc8ddcfd20100
Adam Huss <adamhuss13@gmail.com>
2024-04-16 15:58:09 -0500

    added get all tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,26 +5,32 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', async (req, res) => {
   // find all tags
-  const results = await Tag.findAll();
+  const results = await Tag.findAll({
+    include: {model: Product}
+  });
   res.json(results);
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+
 });
 
 module.exports = router;
